470fa6c089c46fa4b4f2ad3ee185dffa9bc6644b
AlfredoSequeida <alfredosequeida@outlook.com>
2017-05-06 13:45:24 -0700

    created message structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2017 Alfredo Sequeida (alfredosequeida.github.io)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+*/
+
 var rumApp = angular.module('rumApp', ['ngRoute']);
 
 // configure our routes
